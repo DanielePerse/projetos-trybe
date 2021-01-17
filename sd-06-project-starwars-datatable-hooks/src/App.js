3b0884b184453e2175f2d1298ed67daed8f2a524
danieleperse <danieleperse3@gmail.com>
2021-01-17 18:17:09 -0300

    adicionando projeto starwars datatablehooks

---
+++ sd-06-project-starwars-datatable-hooks/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchInput from './components/SearchInput';
+import SearchFilters from './components/SearchFilters';
+import Table from './components/Table';
+import StarWarsProvider from './context/StarWarsProvider';
+
+function App() {
+  return (
+    <StarWarsProvider>
+      <SearchInput />
+      <SearchFilters />
+      <Table />
+    </StarWarsProvider>
+  );
+}
+
+export default App;
