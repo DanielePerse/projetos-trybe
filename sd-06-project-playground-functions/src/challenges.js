c2b3dc42c2175e934b26a81d4ee654fb48f6fff9
danieleperse <danieleperse3@gmail.com>
2021-01-17 17:48:03 -0300

    adicionando projeto playground functions

---
+++ sd-06-project-playground-functions/src/challenges.js
@@ -0,0 +1,166 @@
+// Desafio 1
+function compareTrue(a, b) {
+  return a && b;
+}
+
+// Desafio 2
+function calcArea(base, height) {
+  return (base * height) / 2;
+}
+
+// Desafio 3
+function splitSentence(string) {
+  return string.split(' ');
+}
+
+// Desafio 4
+function concatName(strings) {
+  return `${strings[strings.length - 1]}, ${strings[0]}`;
+}
+
+// Desafio 5
+function footballPoints(wins, ties) {
+  return (wins * 3) + ties;
+}
+
+// Desafio 6
+function highestCount(array) {
+  let maior = 0;
+  let resultado = 0;
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] > maior) {
+      maior = array[i];
+      resultado = 0;
+    }
+    if (maior === array[i]) {
+      resultado += 1;
+    }
+  }
+  return resultado;
+}
+
+
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = Math.abs(cat1 - mouse);
+  let distanciaCat2 = Math.abs(cat2 - mouse);
+
+  if (distanciaCat1 < distanciaCat2) {
+    return 'cat1';
+  } else if (distanciaCat2 < distanciaCat1) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
+}
+
+
+// Desafio 8
+function fizzBuzz(array) {
+  let resultado = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] % 15 === 0) {
+      resultado.push('fizzBuzz');
+    } else if (array[i] % 3 === 0) {
+      resultado.push('fizz');
+    } else if (array[i] % 5 === 0) {
+      resultado.push('buzz');
+    } else {
+      resultado.push('bug!');
+    }
+  }
+  return resultado;
+}
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
+
+// Desafio 9
+function encode(sentence) {
+  let resultado = '';
+  for (let i = 0; i < sentence.length; i += 1) {
+    switch (sentence[i]) {
+      case 'a':
+        resultado += '1';
+        break;
+      case 'e':
+        resultado += '2';
+        break;
+      case 'i':
+        resultado += '3';
+        break;
+      case 'o':
+        resultado += '4';
+        break;
+      case 'u':
+        resultado += '5';
+        break;
+      default:
+        resultado += sentence[i];
+    }
+  }
+  return resultado;
+}
+
+function decode(word) {
+  let result = '';
+  for (let i = 0; i < word.length; i += 1) {
+    switch (word[i]) {
+      case '1':
+        result += 'a';
+        break;
+      case '2':
+        result += 'e';
+        break;
+      case '3':
+        result += 'i';
+        break;
+      case '4':
+        result += 'o';
+        break;
+      case '5':
+        result += 'u';
+        break;
+      default:
+        result += word[i];
+    }
+  }
+  return result;
+}
+
+
+// Desafio 10
+function techList() {
+  // seu código aqui
+}
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
+
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
+}
