2f6ae793048a2bb88b94801dcb57cff31ed75e72
danieleperse <danieleperse3@gmail.com>
2021-04-04 10:28:41 -0300

    add projeto trybeet

---
+++ sd-06-trybeer/front-end/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import AdminOrders from './pages/AdminOrders';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
+import Products from './pages/Products';
+import Profile from './pages/Profile';
+import Register from './pages/Register';
+import Provider from './Context/Provider';
+import Checkout from './pages/Checkout';
+import OrderDetails from './pages/OrderDetails';
+import AdminProfile from './pages/AdminProfile';
+import AdminOrdersDetails from './pages/AdminOrdersDetails';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Provider>
+        <Switch>
+          <Route exact path="/admin/orders/:id" component={ AdminOrdersDetails } />
+          <Route exact path="/admin/orders" component={ AdminOrders } />
+          <Route exact path="/admin/profile" component={ AdminProfile } />
+          <Route exact path="/login" component={ Login } />
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/products" component={ Products } />
+          <Route exact path="/orders" component={ Orders } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/checkout" component={ Checkout } />
+          <Route exact path="/orders/:id" component={ OrderDetails } />
+          <Redirect from="/" to="/login" />
+        </Switch>
+      </Provider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
